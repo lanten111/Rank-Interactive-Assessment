ecabd8c032a0e38cc65da999e8197057e0eeac51
makhado <makhadolucky@gmail.com>
2022-02-14 20:44:22 +0200

    added player repository

---
+++ src/main/java/com/example/rankinteractiveassessment/repository/PlayerRepository.java
@@ -0,0 +1,11 @@
+package com.example.rankinteractiveassessment.repository;
+
+import com.example.rankinteractiveassessment.domain.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+
+    Player getByPlayerId(String playerId);
+}
