b1758920ac10996078a2d1b8ca54c4a083ce13be
makhado <makhadolucky@gmail.com>
2022-02-14 19:41:12 +0200

    initial commit

---
+++ src/test/java/com/example/rankinteractiveassessment/RankInteractiveAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rankinteractiveassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankInteractiveAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
